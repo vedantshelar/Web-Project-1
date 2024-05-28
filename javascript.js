d07d43eb5389c37882a8bfe867ef77d448884e79
vedantshelar <vedantshelar871@gmail.com>
2024-05-28 20:57:58 +0530

    saved

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let userInp = document.querySelector(".userInp");
+let QRCODE = document.querySelector("#QR");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click",()=>{
+
+if(userInp.value!=""){
+let url = `https://api.qrserver.com/v1/create-qr-code/?&data=${userInp.value}`;
+QRCODE.src=url; 
+QRCODE.style.display="block";
+}else{
+    alert("Enter Something is must")
+}
+
+})
